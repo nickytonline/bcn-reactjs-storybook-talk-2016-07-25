33da633f2ecc6fed87cb86098082933081cd318b
Nick Taylor <nick@weareadaptive.com>
2016-07-18 12:19:38 +0200

    initial commit.

---
+++ README.md
@@ -0,0 +1,10 @@
+# BCN ReactJS Talk on React Storybook
+A talk on [React Storybook](https://github.com/kadirahq/react-storybook) that uses [Spectacle](https://github.com/FormidableLabs/spectacle) for the slide deck.
+
+
+# To View The Slide Deck
+* run `npm i`.
+* run  `npm start`.
+* open a browser and hit [http://localhost:3000](http://localhost:3000), and you are ready to roll.
+
+Note: Thanks to the [Spectacle Boilerplate](https://github.com/FormidableLabs/spectacle-boilerplate) for facilitating writing the slide deck.
---
+++ presentation/index.js
@@ -0,0 +1,57 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import {
+  Appear,
+  BlockQuote,
+  Cite,
+  CodePane,
+  Deck,
+  Fill,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  ListItem,
+  List,
+  Markdown,
+  Quote,
+  Slide,
+  Spectacle,
+  Text
+} from "spectacle";
+
+// Import image preloader util
+import preloader from "spectacle/lib/utils/preloader";
+
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+
+// Require CSS
+require("normalize.css");
+require("spectacle/lib/themes/default/index.css");
+// preloader(images);
+
+const theme = createTheme({
+  primary: "#000000"
+});
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Spectacle theme={theme}>
+        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
+          <Slide transition={["zoom"]} bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1}>
+              React Storybook
+            </Heading>
+            <Heading size={1} fit caps>
+              Isolate your React UI Component development from the main app
+            </Heading>
+          </Slide>
+        </Deck>
+      </Spectacle>
+    );
+  }
+}
